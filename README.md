0300b05ca548bd66bb670aaea3f01b48240e23a9
Ty Everett <ty@tyweb.us>
2021-05-25 12:35:56 -0700

    Tutorial #2: Using Action Protocols

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Babbage Hello World
+# Tutorial: Using Action Protocols
 
-This is the companion codebase for the [Babbage Hello World](https://projectbabbage.com/hello-world) tutorial.
+This is the companion codebase for the [Using Action Protocols](https://projectbabbage.com/using-action-protocols) tutorial.
 
 ## Setup
+
+To get started with this code, open your terminal and change directories to the code. Then, run these commands:
 
 ```
 npm i
--- src/App.js
+++ src/App.js
@@ -1,46 +1,129 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { createAction } from '@babbage/sdk';
-import { Button } from '@material-ui/core';
+import { createAction, getPrimarySigningPub } from '@babbage/sdk';
+import { TextField, Button, Typography } from '@material-ui/core';
+import bsv from 'bsv';
+import parapet from 'parapet-js';
 
 function App () {
+  const [postText, setPostText] = useState('')
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const result = await parapet({
+        bridge: '1Prq8NfyBH2mGaGG3jPcbfgH4JXj5aK9sN',
+        request: {
+          type: 'json-query',
+          query: {
+            v: 3,
+            q: {
+              collection: 'hello',
+              find: {},
+              limit: 10
+            }
+          }
+        }
+      })
+      setMessages(result)
+    })()
+  }, [])
+
+  useEffect(() => {
+    let socket
+    (async () => {
+      socket = await parapet({
+        bridge: '1Prq8NfyBH2mGaGG3jPcbfgH4JXj5aK9sN',
+        request: {
+          type: 'socket',
+          query: {
+            v: 3,
+            q: {
+              find: {}
+            }
+          }
+        }
+      })
+      socket.onmessage = async e => {
+        const data = JSON.parse(e.data)
+        if (data.type !== 'message') {
+          return
+        }
+        setMessages(oldMessages => ([
+          ...oldMessages,
+          data.data
+        ]))
+      }
+    })()
+    return () => {
+      if (socket) {
+        socket.close()
+      }
+    }
+  }, [])
+
+  // The function is run when we click the button
   const handleClick = async () => {
+    if (!postText) {
+      alert('No post text!')
+      return
+    }
+    if (postText.length > 512) {
+      alert('HWP messages cannot exceed 512 characters!')
+      return
+    }
+
+    // An HWP sender ID is calculated from the public key
+    const key = await getPrimarySigningPub({
+      path: 'm/1033/10'
+    })
+    const senderID = bsv.Address.fromPublicKey(
+      bsv.HDPublicKey.fromString(key).publicKey
+    ).toString()
+    console.log(`Sender ID: ${senderID}`)
+
     const result = await createAction({
-      // We provide a present-tense description of what is being done
-      description: 'Create my first Action',
-
-      // Here, we just specify which of the users keys to use.
-      // You can ignore this for now, but it will be important later.
+      description: 'Send a message with Hello World Protocol',
       keyName: 'primarySigning',
-      keyPath: 'm/1033/1',
-
-      // Finally, we specify the data for this Action.
-      // This is an array of fields that go into a Bitcoin transaction.
+      keyPath: 'm/1033/10',
       data: [
-        /*
-          All strings need to be in base64 format, so we'll use the built-in 
-          "btoa" function to convert our message.
-        */
-        btoa('Hello Babbage, my name is Ty')
-        // Change this to your own name ^
+        btoa('1He11omzQsAeYa2JUj52sFZRQEsSzPFNZx'),
+        btoa(senderID),
+        btoa(postText)
       ]
     })
 
-    // Finally, let's see what we get when we create the Action!
+    setPostText('')
     console.log(result)
-    alert(`txid: ${result.txid}`)
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <TextField
+          value={postText}
+          onChange={e => setPostText(e.target.value)}
+          placeholder='Write a message'
+          multiline
+          rows={4}
+        />
+        <br />
+        <br />
         <Button
           variant='contained'
           color='primary'
           size='large'
           onClick={handleClick}
         >
-          Create Action
+          Send
         </Button>
+        <br />
+        <br />
+        {messages.map((msg, i) => (
+          <Typography key={i} paragraph>
+            <b>{msg.sender}</b>: {msg.message}
+          </Typography>
+        ))}
       </header>
     </div>
   );
